95415abc4d3ea255d183031adbe020bdb2f295cc
Neil Empey <argus.two.2@gmail.com>
2021-09-19 23:24:28 -0700

    redesign

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+export default function Main() {
+  return (
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container">
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h1 class="masthead-brand">Your not here by accident</h1>
+
+              <br></br>
+            </div>
+          </div>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <br></br>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p class="second-lead">© 2021 neileTech</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Video.js
@@ -0,0 +1,10 @@
+export default function Video() {
+  return (
+    <video autoPlay loop playsInline id="myVideo">
+      <source
+        src="https://res.cloudinary.com/argus2/video/upload/v1632113480/Don_t_wait._The_time_will_never_be_just_right._3_w6wyaf.mp4"
+        type="video/mp4"
+      ></source>
+    </video>
+  );
+}
